7035fb52e8f71d924920661ab019f48f0903d22a
ignw <ignw@ignw-jumphost.ignw.io>
2019-06-03 10:09:51 +0000

    Add a new line in router connect

--- routerconnect.py
+++ routerconnect.py
@@ -2,9 +2,13 @@ import pexpect
 import re
 
 username = 'ignw'
-password = 'ignw'
+password = '1111ignw'
 device_ip = '10.0.0.5'
 
-connection = pexpect.spawn("ssh "+username+"@"+device_ip)
-print(connection)
-print(type(connection))
+#connection = pexpect.spawn("ssh "+username+"@"+device_ip)
+connection = pexpect.spawn(f'ssh {username}@{device_ip}')
+#print(connection)
+#print(type(connection))
+
+connection.expect('Password:')
+connection.sendline(password)
